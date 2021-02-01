9929b759d66d9ba336533e1f6cd55c03d055fc4c
Алексей Лемберский <alexlember@gmail.com>
2021-02-02 00:46:27 +0300

    3 - hash map impl

--- src/test/java/ru/lember/collections/map/HashMapTest.java
+++ src/test/java/ru/lember/collections/map/HashMapTest.java
@@ -8,14 +8,28 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * threshold - The next size value at which to resize (capacity * load factor)
+ *
+ * get O(1)
+ * put O(1)
+ * remove O(1)
+ * contains O(1)
+ */
 public class HashMapTest {
 
     @Test
     void initTest() {
 
-        Map<String, String> map = new HashMap<>();
+        Map<String, String> map = new HashMap<>(4);
         Assertions.assertThat(map).isNotNull();
         Assertions.assertThat(map).isEmpty();
+
+        map.put("1", "A");
+        map.put("2", "A");
+        map.put("3", "A");
+        map.put("4", "A");
+        map.put("5", "A");
 
     }
 }
